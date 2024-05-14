6164818a74be1d1672c58c664a24c986d694ca73
Kaique Brito <165585060+britokaiquee@users.noreply.github.com>
2024-05-14 14:47:57 -0300

    Add files via upload

---
+++ 0.3_Zen_Python.py
@@ -0,0 +1,84 @@
+# Apenas essa parte do Zen (sem os significados) que foi do curso da Udemy.
+
+'''
+The Zen of Python, por Tim Peters
+
+1. Bonito é melhor que feio.
+2. Explícito é melhor que implícito.
+3. Simples é melhor que complexo.
+4. Complexo é melhor que complicado.
+5. Plano é melhor que aglomerado.
+6. Esparso é melhor que denso.
+7. Legibilidade conta.
+8. Casos especiais não são especiais o bastante para quebrar as regras.
+9. Embora a praticidade vença a pureza.
+10. Erros nunca devem passar silenciosamente.
+11. A menos que sejam explicitamente silenciados.
+12. Diante da ambiguidade, recuse a tentação de adivinhar.
+13. Deve haver um -- e só um -- modo óbvio para fazer algo.
+14. Embora esse modo possa não ser óbvio à primeira vista a menos que você seja
+holandês.
+15. Agora é melhor que nunca.
+16. Embora nunca frequentemente seja melhor que *exatamente* agora.
+17. Se a implementação é difícil de explicar, é uma má ideia.
+18. Se a implementação é fácil de explicar, pode ser uma boa ideia.
+19. Namespaces são uma grande ideia -- vamos fazer mais dessas!
+
+
+
+Significados de cada frase:
+
+1. Bonito é melhor que feio: Prefira código esteticamente agradável e fácil de
+   ler em vez de código confuso ou desorganizado.
+2. Explícito é melhor que implícito: É preferível que o código seja claro e
+   direto em vez de depender de suposições ou comportamentos ocultos.
+3. Simples é melhor que complexo: Dê preferência a soluções simples e diretas
+   em vez de soluções complicadas ou intricadas.
+4. Complexo é melhor que complicado: Se a situação exigir complexidade, é
+   melhor ser claro e expressivo do que complicado e difícil de entender.
+5. Plano é melhor que aninhado: Evite aninhar blocos de código
+   profundamente, pois isso pode tornar o código difícil de entender. Prefira
+   estruturas planas sempre que possível.
+6. Esparso é melhor que denso: Prefira código espaçoso e bem espaçado em vez
+   de código denso e comprimido, o que pode ser difícil de ler.
+7. Legibilidade conta: A clareza e a facilidade de compreensão do código são
+   importantes. Priorize a legibilidade para que outros desenvolvedores possam
+   entender facilmente o que o código faz.
+8. Casos especiais não são especiais o suficiente para quebrar as regras: Evite
+   criar exceções ou regras especiais desnecessárias que possam tornar o código
+   mais complicado. Siga os princípios gerais sempre que possível.
+9. Embora praticidade vença a pureza: Às vezes, é mais importante ser prático e
+   eficaz do que aderir rigidamente à perfeição ou à pureza conceitual.
+10. Erros nunca devem passar em silêncio: Não ignore erros ou exceções. Eles
+    devem ser tratados de forma apropriada para evitar comportamentos
+    inesperados.
+11. A menos que sejam explicitamente silenciados: No entanto, há casos em que
+    é apropriado suprimir erros ou exceções, desde que seja feito explicitamente
+    e de forma controlada.
+12. Diante da ambiguidade, recuse a tentação de adivinhar: Se o significado de
+    uma operação ou comportamento não for claro, é melhor ser explícito e evitar
+    suposições.
+13. Deveria haver um - e preferencialmente só um - modo óbvio de fazer isso:
+    Prefira soluções simples e diretas em vez de várias maneiras complicadas de
+    realizar a mesma tarefa.
+14. Embora esse modo possa não ser óbvio à primeira vista, a menos que você
+    seja holandês: Reconhece que a simplicidade pode ser uma questão de
+    perspectiva e experiência. Algumas soluções podem não ser óbvias para todos,
+    mas devem ser acessíveis e claras para a maioria.
+15. Agora é melhor que nunca: É melhor fazer as coisas de forma oportuna e
+    rápida do que adiar indefinidamente. Ação imediata é preferível à
+    procrastinação.
+16. Embora nunca seja muitas vezes melhor que *agora*: No entanto, se uma ação
+    não pode ser realizada imediatamente, é melhor fazê-la tarde do que nunca.
+    Ainda assim, a procrastinação não é uma boa prática.
+17. Se a implementação é difícil de explicar, é uma má ideia: Se uma solução é
+    complicada demais para ser facilmente explicada, é provavelmente uma má
+    abordagem. Prefira soluções simples e claras.
+18. Se a implementação é fácil de explicar, pode ser uma boa ideia: Se uma
+    solução é simples e fácil de explicar, é mais provável que seja uma boa
+    abordagem. A clareza na explicação é um bom indicador de uma implementação
+    sólida.
+19. Namespaces são uma ótima ideia - vamos fazer mais dessas!: O uso de
+    namespaces (espaços de nomes) para evitar conflitos de nomes é uma prática
+    recomendada. Encoraja a modularização e a organização do código.
+'''
